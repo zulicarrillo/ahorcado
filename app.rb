6faa44678e48f89360343120652c8d5cd644e9fb
Kleer LA <kleer@kleer-sandbox.(none)>
2016-07-09 11:09:31 -0300

    g

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ end
 get '/' do
     @letra= @@ahorcado.obtenerCantidadLetras
 	@palabra= @@ahorcado.obtenerPalabra
-	#@palabraJugador=@@ahorcado.obtenerPalabraJugador
+	@palabraJugador=@@ahorcado.obtenerPalabraJugador
 	erb:ahorcado
 end
 
--- lib/ahorcado.rb
+++ lib/ahorcado.rb
@@ -4,7 +4,7 @@ class Ahorcado
 	@palabra=@ary.sample
 	#@palabra="auto"
 	@cantidadLetras= @palabra.length
-	@palabraJugador=""
+	@palabraJugador="autos"
 	iniciarPalabraJugador()
 	end
 	def obtenerPalabra()
